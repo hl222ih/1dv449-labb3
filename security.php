fecbd0a925e42dc88cbd97b46d0facb87a1b5e8b
hl222ih <hl222ih@student.lnu.se>
2014-12-10 01:49:35 +0100

    startsida med inlänkade resurser som jquery och bootstrap via CDN

---
+++ security.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Eftersom applikationen saknar inloggning så är detta bara ett sätt att något försvåra direktåtkomst till resurser
+ */
+
+function getToken() {
+    if (!isset($_SESSION['token'])) {
+        $_SESSION['token'] = base64_encode( openssl_random_pseudo_bytes(32));
+    }
+    return $_SESSION['token'];
+}
+
+function checkToken() {
+    $isAuthenticated = false;
+    if (isset($_SERVER['HTTP_X_AUTH_TOKEN'])) {
+        $isAuthenticated = ($_SERVER['HTTP_X_AUTH_TOKEN'] == getToken());
+    }
+    return $isAuthenticated;
+}
