630450d4cd9d4cfd6504a28d48d3d5101ccfdf28
hl222ih <hl222ih@student.lnu.se>
2014-12-11 04:12:54 +0100

    påbörja bearbetning av data

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 	require_once("security.php");
-    header("Content-Security-Policy: script-src 'self' http://ajax.googleapis.com http://maxcdn.bootstrapcdn.com");
+/*    header("Content-Security-Policy: script-src 'self' http://ajax.googleapis.com http://maxcdn.bootstrapcdn.com https://maps.googleapis.com https://maps.gstatic.com");*/
 ?>
 <!DOCTYPE html>
 <html lang="sv">
@@ -12,6 +12,7 @@
     <title>1dv449 labb3 (hl222ih)</title>
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php getGoogleMapsApiKey() ?>&v=3.18&language=sv&region=SE"></script>
 
 </head>
@@ -20,10 +21,11 @@
 <div id="container">
 
     <h1>nothing here yet...</h1>
-    <div id="content">
-    </div>
+    <ul id="content">
+    </ul>
+    <div id="map"></div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="js/app.js"></script>
+    <script src="js/app.js?random=<?php echo rand(1,10000); ?>"></script>
 </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ var HL = HL || {};
 
 HL = {
     GoogleMaps: function() {
-        alert("map function!");
+        var map = new google.maps.Map($('#map')[0], {
+           zoom: 6,
+           center: {lat: 60, lng: 14},
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
     },
     GetMessages: function() {
         $.ajax({
@@ -11,16 +15,45 @@ HL = {
             headers: {
                 'X-Auth-Token' : $("meta[name='token']").attr("content")
             }
-        }).done(function(data) {
+        }).done(function(data, textStatus, jqXHR) {
             var parsedData = JSON.parse(data);
             if (parsedData.error) {
                 $("#content").append('<p>' + parsedData.error  + '</p>');
             } else {
+                HL.Messages = parsedData['messages'];
                 $("#content").append('<p>' + data + '</p>'); //tillfällig utskrivning av rådata
+                HL.ShowMessages();
             }
-        }).fail(function(data) {
-            $("#content").append('<p>' + data.status + ' ' + data.statusText + '</p>');
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            $("#content").append('<p>' + jqXHR.status + ' ' + textStatus + '</p>');
         });
+    },
+    Messages: [],
+    FilteredMessages: [],
+    ShowMessages: function(subCategory) {
+        if (subCategory) {
+            HL.FilteredMessages = this.Messages.filter(function(message) { return message['subcategory'] === subCategory });
+        } else {
+            HL.FilteredMessages = $.extend(true, [], HL.Messages);
+        }
+        HL.ProcessMessages();
+        alert(JSON.stringify(HL.FilteredMessages));
+    },
+    ProcessMessages: function() {
+        var i = 0;
+        var $contentNode = $("#content");
+        $contentNode.empty();
+//        $contentNode.append('<p>' + data + '</p>');
+
+        for (i; i < HL.FilteredMessages.length; i++) {
+            $contentNode.append('<li>' + HL.FilteredMessages[i]['subcategory'] + '</li>');
+        }
+    },
+    RenderMessage: function() {
+
+    },
+    AddMarker: function() {
+
     }
 };
 
